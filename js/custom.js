62463d3298a4915a6df8cac0e721ba6f5efe6fc2
Brandi Tungett <bmtung01@louisville.edu>
2019-07-25 14:30:33 -0400

    updated javascript with David's help

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,37 @@
 // this is my form validation code
 (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // the following targets the correct form, I used a class to get us there
-    var forms = document.getElementsByClassName('validation-required');
-      // Loop over them and prevent submission, will display messages under required fields if not validated
-    var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-        }, false);
-    });
-    }, false);
-})();
+  'use strict';
+  window.addEventListener('load', function() {
+    // the following targets the correct form, I used a class to get us there
+  var forms = document.getElementsByClassName('validation-required');
 
+  // Collect inputs to clear on successful submit
+  var inputs = document.getElementsByClassName('form-control');
+
+    // Loop over them and prevent submission, will display messages under required fields if not validated
+  var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        // Contact form submission error
+        if (form.checkValidity() === false) {
+            // The fields in the form are being checked for being filled dependent on format (text/email), if they are not filled it will loop
+            form.classList.add('was-validated');
+        }
+        // Contact form submit is successful, if validation is true
+        else {
+          // Show user an alert
+          alert('Thanks for reaching out!');
+
+          // Clear input fields
+          for (var i = 0; i < inputs.length; i++) {
+            inputs[i].value = "";
+          }
+        }
+
+        // Stop form from submitting
+        event.preventDefault();
+        event.stopPropagation();
+      }, false);
+  });
+  }, false);
+})();
 
